6651693f3fb271a4038f6741e30a46d79d18ccca
bbherrero <herrero.o.b@gmail.com>
2025-09-09 11:25:06 +0200

    Feat: create main and chidren routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,47 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: 'events',
+        component: () => import('layouts/EventsLayout.vue'),
+        children: [
+          {
+            path: '',
+            component: () => import('pages/EventsPage.vue'),
+            meta: { title: 'Events' },
+          },
+          {
+            path: ':id',
+            component: () => import('pages/EventDetailsPage.vue'),
+            meta: { title: 'Event Details' },
+          },
+        ],
+      },
+      {
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            component: () => import('pages/ProfilePage.vue'),
+            meta: { title: 'Profile' },
+          },
+          {
+            path: 'about',
+            component: () => import('pages/AboutPage.vue'),
+            meta: { title: 'About' },
+          },
+          {
+            path: 'disclaimer',
+            component: () => import('pages/DisclaimerPage.vue'),
+            meta: { title: 'Disclaimer' },
+          },
+        ],
+      },
+    ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
