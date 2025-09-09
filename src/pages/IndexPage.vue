fc0a35d589c0d93fb92abfd4c5ab60f77fc341b7
bbherrero <herrero.o.b@gmail.com>
2025-09-09 11:31:17 +0200

    Style: update app logo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,17 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
-  </q-page>
-</template>
-
 <script setup>
 //
 </script>
+
+<template>
+  <q-page class="column flex flex-center bg-gradient">
+    <img alt="IH-TSUSY logo" src="~assets/ihtsusy-logo.png" style="width: 120px; height: auto" />
+    <h1 class="text-h5 text-center q-mt-md"> IH-Tsunami System </h1>
+    <q-btn to="/events" rounded color="secondary" label="Go to Events Page" class="q-mt-md text-capitalize" />
+  </q-page>
+</template>
+
+<style lang="css" scoped>
+.bg-gradient {
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(17, 131, 255, 0.15) 50.84%, rgba(17, 131, 255, 0.25) 101.68%);
+}
+</style>
