01a607a3b7a221df549aca621b19ed21f7f1c5a7
bbherrero <herrero.o.b@gmail.com>
2025-09-09 11:29:29 +0200

    Feat: create events layout

---
+++ src/layouts/EventsLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    Header
+  </div>
+  <div class="map">
+    Map
+  </div>
+  <div class="content">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </div>
+</template>
+<style lang="scss">
+.header {
+  height: 50px;
+  width: 100%;
+  background-color: grey;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5em;
+  z-index: 10;
+}
+
+.map {
+  position: absolute;
+  top: 50px;
+  height: calc(100% - 50px);
+  width: 100%;
+  background-color: lightblue;
+  z-index: -5;
+}
+
+.content {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: auto;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+</style>
