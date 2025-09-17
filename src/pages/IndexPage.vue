d7e5af4c6b4710b46adb402a0b7580fefe93e040
bbherrero <herrero.o.b@gmail.com>
2025-09-17 13:28:43 +0200

    Feat: modify push notification event to appear when enter the app

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,41 @@
 <script setup>
+import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+
 const router = useRouter()
-const enterApp = () => {
+
+const requestNotificationPermission = async () => {
+  // Verify if browser supports notifications
+  if (!('Notification' in window)) {
+    console.log('Notifications not supported.')
+    return
+  }
+
   const promise = Notification.requestPermission()
-  promise.then((permission) => {
+  await promise.then((permission) => {
     if (permission === 'granted') {
       console.log('Notification permission granted.')
       new Notification('TSUSY', { body: 'This is a test notification.' })
     } else {
       console.log('Notification permission denied.')
     }
-  }).finally(() => {
-    // Navigate to events page after handling permission
-    router.push('/events')
   })
 };
+
+onMounted(async () => {
+  await requestNotificationPermission()
+  // TODO: request here data needed for the app before entering
+
+  router.push('/events')
+
+})
 </script>
 
 <template>
   <q-page class="column flex flex-center bg-gradient">
     <img alt="IH-TSUSY logo" src="~assets/ihtsusy-logo.png" style="width: 120px; height: auto" />
     <h1 class="text-h5 text-center q-mt-md"> IH-Tsunami System </h1>
-    <q-btn @click="enterApp" rounded color="secondary" label="Go to Events Page" class="q-mt-md text-capitalize" />
+    <!-- <q-btn @click="enterApp" rounded color="secondary" label="Go to Events Page" class="q-mt-md text-capitalize" /> -->
   </q-page>
 </template>
 
